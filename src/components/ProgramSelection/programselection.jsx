59fb9a5324e429b3586722d44a111a42e9e1efca
Rafael Silva <rafsilva004@gmail.com>
2023-10-18 20:34:47 -0500

    styled home page and buttons

--- src/components/ProgramSelection/programselection.jsx
+++ src/components/ProgramSelection/programselection.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
+import './ProgramSelection.css';
 
 function ProgramSelection() {
+
+
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-
+      <h1>Welcome, {user.username}!</h1>
+      <center>
+      <h1>Select Your Program</h1>
+      </center>
+      
       <center>
         <button
           type="button"
-          className="btn btn_asLink"
+          className="threeday"
           onClick={() => {
             history.push('/registration');
           }}
@@ -26,7 +29,7 @@ function ProgramSelection() {
         <center>
         <button
           type="button"
-          className="btn btn_asLink"
+          className="fourday"
           onClick={() => {
             history.push('/registration');
           }}
@@ -37,7 +40,7 @@ function ProgramSelection() {
           <center>
         <button
           type="button"
-          className="btn btn_asLink"
+          className="fiveday"
           onClick={() => {
             history.push('/registration');
           }}
