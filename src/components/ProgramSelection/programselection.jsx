6132a5f5a4101bface5cd777d627205caeb6e202
Rafael Silva <rafsilva004@gmail.com>
2023-10-18 19:21:06 -0500

    changed layout of buttons

--- src/components/ProgramSelection/programselection.jsx
+++ src/components/ProgramSelection/programselection.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function ProgramSelection() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
+    
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
-        <button type= "button" className=''>3-Day Program</button>
-        <button>4-Day Program</button>
-        <button>5-Day Program</button>
-    </div>
+
+      <center>
+        <button
+          type="button"
+          className="btn btn_asLink"
+          onClick={() => {
+            history.push('/registration');
+          }}
+        >
+          3-Day Program
+        </button>
+        </center>
+        <center>
+        <button
+          type="button"
+          className="btn btn_asLink"
+          onClick={() => {
+            history.push('/registration');
+          }}
+        >
+          4-Day Program
+          </button>
+          </center>
+          <center>
+        <button
+          type="button"
+          className="btn btn_asLink"
+          onClick={() => {
+            history.push('/registration');
+          }}
+        >
+          5-Day Program
+        </button>
+        </center>  
+          </div>
   );
 }
-
 // this allows us to use <App /> in index.js
 export default ProgramSelection;
