aa22a792bf086636463adaae3167cc48c08e8d98
Rafael Silva <rafsilva004@gmail.com>
2023-10-25 19:47:25 -0500

    worked on four day program page

--- src/components/ThreeDayProgram/ThreeDayProgram.jsx
+++ src/components/ThreeDayProgram/ThreeDayProgram.jsx
@@ -4,71 +4,71 @@ import './ThreeDayProgram.css';
 
 
 function ThreeDayProgram() {
-    
-      const [selectedButton, setSelectedButton] = useState(null);
-      const [isWorkoutStarted, setWorkoutStarted] = useState(false);
 
-    
-      const history = useHistory();
-      const buttons = ["Push","Pull","Legs"];
-    
-      const exercises = [
+    const [selectedButton, setSelectedButton] = useState(null);
+    const [isWorkoutStarted, setWorkoutStarted] = useState(false);
+
+
+    const history = useHistory();
+    const buttons = ["Push", "Pull", "Legs"];
+
+    const exercises = [
         ["Exercise 1", "Exercise 2", "Exercise 3"],
         ["Exercise 4", "Exercise 5", "Exercise 6"],
         ["Exercise 7", "Exercise 8", "Exercise 9"],
-      ];
-    
-      const handleButtonClick = (index) => {
+    ];
+
+    const handleButtonClick = (index) => {
         setSelectedButton(index);
         setWorkoutStarted(false); // Reset workout state when a new button is clicked
-      };
-    
-      const handleStartWorkout = () => {
+    };
+
+    const handleStartWorkout = () => {
         setWorkoutStarted(true);
-      };
-    
-      return (
+    };
+
+    return (
         <div className="exercise-page">
-          <div className="header">
-            3-Day Program
-          </div>
-          <div className="button-container">
-            {buttons.map((button, index) => (
-              <button
-                key={index}
-                className={`exercise-button ${selectedButton === index ? "active" : ""}`}
-                onClick={() => handleButtonClick(index)}
-              >
-                {button}
-              </button>
-            ))}
-          </div>
-          <div className="exercise-list-container">
-            {selectedButton !== null && !isWorkoutStarted && (
-              <div>
-                <ul className="exercise-list">
-                  {exercises[selectedButton].map((exercise, index) => (
-                    <li key={index}>{exercise}</li>
-                  ))}
-                </ul>
-                <button
-                  className="start-button"
-                  onClick={handleStartWorkout}
-                >
-                  Start Workout
-                </button>
-              </div>
-            )}
-            {isWorkoutStarted && (
-              <div className="workout-in-progress">
-                <p>Workout in progress...</p>
-                {/* Add workout progress components here */}
-              </div>
-            )}
-          </div>
+            <div className="header">
+                3-Day Program
+            </div>
+            <div className="button-container">
+                {buttons.map((button, index) => (
+                    <button
+                        key={index}
+                        className={`exercise-button ${selectedButton === index ? "active" : ""}`}
+                        onClick={() => handleButtonClick(index)}
+                    >
+                        {button}
+                    </button>
+                ))}
+            </div>
+            <div className="exercise-list-container">
+                {selectedButton !== null && !isWorkoutStarted && (
+                    <div>
+                        <ul className="exercise-list">
+                            {exercises[selectedButton].map((exercise, index) => (
+                                <li key={index}>{exercise}</li>
+                            ))}
+                        </ul>
+                        <button
+                            className="start-button"
+                            onClick={handleStartWorkout}
+                        >
+                            Start Workout
+                        </button>
+                    </div>
+                )}
+                {isWorkoutStarted && (
+                    <div className="workout-in-progress">
+                        <p>Workout in progress...</p>
+                        {/* Add workout progress components here */}
+                    </div>
+                )}
+            </div>
         </div>
-      );
-            }
-    
-    
+    );
+}
+
+
 export default ThreeDayProgram;
