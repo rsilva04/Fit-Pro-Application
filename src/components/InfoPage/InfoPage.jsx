65a04370832cfc43be60414fabdc1d45057e6452
Rafael Silva <rafsilva004@gmail.com>
2023-11-08 14:43:19 -0600

    bolded technologies

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 function InfoPage() {
   const listItems = [
+    'JAVASCRIPT',
     'REACT.JS',
     'NODE.JS',
     'EXPRESS',
@@ -14,7 +15,7 @@ function InfoPage() {
 
   return (
     <div className="container" style={{ textAlign: 'center' }}>
-      <p style={{ margin: '0', padding: '0', fontSize: '40px' }}>Technologies:</p>
+      <p style={{ margin: '0', padding: '0', fontSize: '40px', fontWeight: 'bold' }}>Technologies:</p>
       <div>
         {listItems.map((item, index) => (
           <p key={index} style={{ margin: '10px 0', padding: '0', fontSize: '30px' }}>
