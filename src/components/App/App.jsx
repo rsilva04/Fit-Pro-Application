b3c049a377bec245a83569ce231bd07bec0017f9
Rafael Silva <rafsilva004@gmail.com>
2023-11-14 16:22:29 -0600

    edit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,7 @@ import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
-import ProgramSelection from '../programselection/programselection';
+import ProgramSelection from '../ProgramSelection/programselection';
 import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
